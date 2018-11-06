f30cc2aa36fa71f0caf19f44c4d39876693988a3
Michacy45 <pes.mich@gmail.com>
2018-11-06 11:02:56 +0100

    wtorek rano

--- src/Pliki.java
+++ src/Pliki.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,19 +10,35 @@ public class Pliki {
     public String readFile(String pathFile) {
         String evr = new String();
         String error = new String();
-        Path sciezka = Paths.get(pathFile);
+        //Path sciezka = Paths.get(pathFile);
+       // byte[] trescByte = new byte[ ]
+       // FileInputStream inStream
+
+        String data="";
+
 
         try {
-            byte[] tab = Files.readAllBytes(sciezka);
-            evr = new String(tab, "UTF-8");
+            //byte[] tab = Files.readAllBytes(sciezka);
+            //evr = new String(tab, "UTF-8");
+            data=new String(Files.readAllBytes(Paths.get(pathFile)));
         } catch (FileNotFoundException var5) {
             error = "Błąd";
             return error;
         } catch (IOException var6) {
-            error = "Nie podałeś ścieżki";
+            error = "zła ścieżka";
             return error;
         }
 
-        return evr;
+       // return evr;
+        return data;
+    }
+    public void writeFile(String pathFile, byte[] cipher) throws IOException {
+
+        Path path = Paths.get(pathFile);
+            Files.write(path, cipher);
+
+
+
+
     }
 }
